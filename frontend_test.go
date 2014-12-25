7f528c5149a35bd18a59ed3f69e1d0c185f64203
Hariharan Srinath <srinathh@gmail.com>
2014-12-25 22:57:21 +0530

    fully functional client and frontend for V1 with all tests & fake backend

--- frontend_test.go
+++ frontend_test.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -58,4 +61,62 @@ func TestServeTree(t *testing.T) {
 		}
 	}
 
+}
+
+type FailBackend struct {
+	fail    bool
+	comment Comment
+}
+
+func (f *FailBackend) isFail() error {
+	if f.fail {
+		return fmt.Errorf("Random Failure in FailBackend")
+	}
+	return nil
+}
+
+func (f *FailBackend) ListNodeIDs() ([]string, error) {
+	return []string{}, f.isFail()
+}
+
+func (f *FailBackend) GetNode(id string) (Node, error) {
+	return Node{}, f.isFail()
+}
+
+func (f *FailBackend) StartNode(id string) error {
+	return f.isFail()
+}
+
+func (f *FailBackend) EditComment(id string, c Comment) error {
+	if f.isFail() != nil {
+		return f.isFail()
+	}
+	if !reflect.DeepEqual(c, f.comment) {
+		return fmt.Errorf("Comment did not match what was expected\nwant\n%v\nGot\n%v\n", f.comment, c)
+	}
+	return nil
+}
+
+func TestEditComment(t *testing.T) {
+
+	const text = "### Test Data for comments ###\nChecking if this is `correctly` captured"
+
+	data := url.Values{}
+	data.Add("commentid", "CREATE")
+	data.Add("text", text)
+	data.Add("nodeid", "0")
+
+	r, err := http.NewRequest("POST", "/editcomment", strings.NewReader(data.Encode()))
+	if err != nil {
+		t.Fatalf("Error setting up request %s", err)
+	}
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	w := httptest.NewRecorder()
+	failbackend := &FailBackend{false, Comment{Id: "CREATE", ModTime: "", Text: text}}
+	frontend := NewFrontEndServer(failbackend)
+	frontend.EditComment(w, r)
+	if w.Code != http.StatusOK {
+		t.Fatalf("Error in TestEditComment")
+	}
 }
