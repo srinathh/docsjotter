88321b2d6d56fbb368d2b917602b876597aa6708
Hariharan Srinath <srinathh@gmail.com>
2015-01-13 11:26:33 +0530

    simplified main server setup

--- server.go
+++ server.go
@@ -9,37 +9,18 @@ import (
 	"net/http"
 )
 
-//struct FrontEnd stores references to different http.HandlerFunc that will be bound to routes
-//by the PowerDocs server. This enables easy swapping of FrontEnd handlers in a piecemeal
-//fashion for testing and development. PowerDocs uses github.com/justinas/alice to chain middleware
-type AppHandlers struct {
-	ServeStatic http.HandlerFunc //HandlerFunc to serve the static assets like javascript and css
-	ServeIndex  http.HandlerFunc //HandlerFunc to serve index page
-	ServeTree   http.HandlerFunc
-	ServeNode   http.HandlerFunc
-	StartNode   http.HandlerFunc
-	EditComment http.HandlerFunc
-	MiddleWare  alice.Chain
-}
-
 func main() {
 
 	config := LoadConfig("./docsjotter.toml")
 
-	//setup the middleware
-	var apphandlers AppHandlers
-	apphandlers.MiddleWare = alice.New(LogHandler)
+	middleware := alice.New(LogHandler)
 
-	//setup the Resource and index Handlers
 	staticserver, err := NewStaticServer(config.Resdir)
 	if err != nil {
 		log.Fatalf("Error configuring static server : %s", err)
 	}
-	apphandlers.ServeStatic = staticserver.ServeHTTP
-	apphandlers.ServeIndex = staticserver.ServeHTTP
 
 	var bk structs.BackEnd
-
 	switch config.Mode {
 	case "mocktest":
 		bk = mockbackends.NewFakeBackend()
@@ -49,20 +30,11 @@ func main() {
 
 	f := NewFrontEndServer(bk)
 
-	//we're doing this rather than just use an interface to enable components to be
-	//swapped out on the fly
-	apphandlers.ServeTree = f.ServeTree
-	apphandlers.ServeNode = f.ServeNode
-	apphandlers.StartNode = f.StartNode
-	apphandlers.EditComment = f.EditComment
-
-	//Setting up te resource Handler	s
-	//http.Handle("/res/", staticserver)
-	http.Handle("/", apphandlers.MiddleWare.ThenFunc(apphandlers.ServeStatic))
-	http.Handle("/servetree", apphandlers.MiddleWare.ThenFunc(apphandlers.ServeTree))
-	http.Handle("/servenode", apphandlers.MiddleWare.ThenFunc(apphandlers.ServeNode))
-	http.Handle("/startnode", apphandlers.MiddleWare.ThenFunc(apphandlers.StartNode))
-	http.Handle("/editcomment", apphandlers.MiddleWare.ThenFunc(apphandlers.EditComment))
+	http.Handle("/", middleware.Then(staticserver))
+	http.Handle("/servetree", middleware.ThenFunc(f.ServeTree))
+	http.Handle("/servenode", middleware.ThenFunc(f.ServeNode))
+	http.Handle("/startnode", middleware.ThenFunc(f.StartNode))
+	http.Handle("/editcomment", middleware.ThenFunc(f.EditComment))
 
 	//Start the service
 	log.Printf("Serving on %s\n", config.Http)
